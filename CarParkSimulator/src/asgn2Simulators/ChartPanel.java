215f9945fbd8b62cad28a311fca4497d5250c1e9
bec434 <rebeccaz434@hotmail.com>
2014-05-27 22:46:00 +1000

    Making additions to ChartPanel.java

--- CarParkSimulator/src/asgn2Simulators/ChartPanel.java
+++ CarParkSimulator/src/asgn2Simulators/ChartPanel.java
@@ -20,15 +20,21 @@ import org.jfree.data.xy.XYDataset;
 
 /**
 *
-* @author Phil
+* @author Rebecca Zanchetta (n8300941)
+* 		  and Brad Vose (n8280282)
 */
 public class ChartPanel extends org.jfree.chart.ChartPanel {
     private final String TITLE = "Car Park Simulation Results";
     
     private TimeSeries vehTotal;
+    private TimeSeries curTotal;
     private TimeSeries carTotal;
+    private TimeSeries scarTotal;
     private TimeSeries mcTotal;
-    private TimeSeries scTotal;
+    private TimeSeries queueTotal;
+    private TimeSeries archTotal;
+    private TimeSeries dissTotal;
+    
     private Calendar cal = GregorianCalendar.getInstance();
     
     public ChartPanel() {
@@ -44,22 +50,30 @@ public class ChartPanel extends org.jfree.chart.ChartPanel {
     // Sets up the different series used to plot the graph
     private void setUpSeries()
     {
-        vehTotal = new TimeSeries("Total Vehicles");
-        carTotal = new TimeSeries("Total Cars");
-        scTotal = new TimeSeries("Total Small Cars");
-        mcTotal = new TimeSeries("MotorCycles");
+    	vehTotal = new TimeSeries("Total Vehicles to Date");
+    	curTotal = new TimeSeries("Currently Parked Vehicles");
+    	carTotal = new TimeSeries("Total Cars in Car Park");
+    	scarTotal = new TimeSeries("Total Small Cars in Car Park");
+    	mcTotal = new TimeSeries("Total MotorCycles in Car Park");
+    	queueTotal = new TimeSeries("Number of Queued Vehicles");
+    	archTotal = new TimeSeries("Number Archived");
+    	dissTotal = new TimeSeries("Number Dissatisfied");
     }
     
     // Adds datapoints for a particular moment intime to the graph
-    public void addDataForGivenTimePoint( int time, int nbCars, int nbSmallCars, int nbMotorCycles, int total)
+    public void addDataForGivenTimePoint( int time, int dissatisfied, int archived, int queue, int nbCars, int nbSmallCars, int nbMotorCycles, int current, int total)
     {
         cal.set(2014,0,1,6, time);
         Date timePoint = cal.getTime();
         
-        scTotal.add(new Minute(timePoint), nbSmallCars);
+        dissTotal.add(new Minute(timePoint), dissatisfied);
+        archTotal.add(new Minute(timePoint), archived);
+        queueTotal.add(new Minute(timePoint), queue);
         mcTotal.add(new Minute(timePoint), nbMotorCycles);
-carTotal.add(new Minute(timePoint),nbCars);
-vehTotal.add(new Minute(timePoint), total);
+        scarTotal.add(new Minute(timePoint), nbSmallCars);
+        carTotal.add(new Minute(timePoint),nbCars);
+        curTotal.add(new Minute(timePoint), current);
+        vehTotal.add(new Minute(timePoint), total);
     }
     
     // Generate the chart once the car park simulation is completed
@@ -75,9 +89,13 @@ vehTotal.add(new Minute(timePoint), total);
     {
         TimeSeriesCollection tsc = new TimeSeriesCollection();
         tsc.addSeries(vehTotal);
+        tsc.addSeries(curTotal);
         tsc.addSeries(carTotal);
-        tsc.addSeries(scTotal);
+        tsc.addSeries(scarTotal);
         tsc.addSeries(mcTotal);
+        tsc.addSeries(queueTotal);
+        tsc.addSeries(archTotal);
+        tsc.addSeries(dissTotal);
        
         return tsc;
     }
